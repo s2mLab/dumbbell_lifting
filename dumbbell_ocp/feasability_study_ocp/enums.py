c9d7db728c413bd1e4dbf4d7466ca4b7d2589863
Pariterre <pariterre@hotmail.com>
2022-08-04 15:49:39 -0400

    Copy of the ViolinOptimalControl analyses

---
+++ dumbbell_ocp/feasability_study_ocp/enums.py
@@ -0,0 +1,37 @@
+from typing import Union
+from enum import Enum, auto
+
+from bioptim import DynamicsFcn as BioptimDynamicsFcn
+
+
+class PlotOptions:
+    def __init__(
+        self,
+        title: str,
+        legend_indices: Union[tuple[bool, ...], None],
+        options: tuple[dict, ...],
+        to_degrees: bool,
+        maximize: bool = False,
+        save_path: Union[tuple[str, ...], None] = None,
+    ):
+        self.title = title
+        self.legend_indices = legend_indices
+        self.options = options
+        self.to_degrees = to_degrees
+        self.maximize = maximize
+        self.save_path = save_path
+
+
+class DataType(Enum):
+    STATES = "states"
+    CONTROLS = "controls"
+
+
+class DynamicsFcn(Enum):
+    TORQUE_DRIVEN = BioptimDynamicsFcn.TORQUE_DRIVEN
+    MUSCLE_DRIVEN = BioptimDynamicsFcn.MUSCLE_DRIVEN
+
+
+class FatigableStructure(Enum):
+    MUSCLES = auto()
+    JOINTS = auto()
