864bc2c19a10d646e1a79cab1d810e0f7adb3d07
Ipuch <puchaud.pierre@gmail.com>
2022-08-24 08:02:08 -0400

    switching to compute time in the custom analysis

--- fatigue_direct_integration/main.py
+++ fatigue_direct_integration/main.py
@@ -5,8 +5,8 @@ from studies import Study
 def main():
     # Define the studies to perform
     all_studies = (
-        Study.STUDY1_STABILIZER_EFFECT_LONG_TIME,
         Study.STUDY1_STABILIZER_EFFECT_SHORT_TIME,
+        Study.STUDY1_STABILIZER_EFFECT_LONG_TIME,
         Study.STUDY2_STABILIZER_EFFECT_SAME_START,
         Study.STUDY3_STABILIZER_EFFECT_SLIGHTLYBAD_START,
     )
